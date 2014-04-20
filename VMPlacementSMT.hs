2894375413690870e32593096cf59a3f369bf5bb
kau <kau@kau-Studio-XPS-1647>
2014-04-20 18:56:28 +0200

    first commit

---
+++ VMPlacementSMT.hs
@@ -0,0 +1,86 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
+
+import Data.SBV
+import Data.Map (Map, fromList, elems, keys)
+import qualified Data.Map as M
+import Data.Maybe
+import Control.Applicative
+
+--concrete IDs for VMs and servers
+type VMID = Integer
+type SID = Integer
+
+--Symbolic annotation. A prefix 'S' indicates that it's a symbolic value
+type S = SBV
+
+--symbolic IDs servers
+type SSID = S SID
+
+--a type synonim for the location of VMs on servers
+type VMLoc a = Map VMID a
+
+--A VM is just a name and a cpuDemand
+data VM = VM { vmName :: String,
+               cpuDemand :: Integer}
+
+--a server has got a name and a certain amount of free CPU
+data Server = Server { serverName :: String,
+                       cpus :: Integer}
+
+--list of VMs
+vms :: VMLoc VM
+vms = fromList $ zip [0..] [VM "VM1" 100, VM "VM2" 50, VM "VM3" 15]
+
+--list of servers
+servers :: Map SID Server
+servers = fromList $ zip [0..] [Server "Server1" 100, Server "Server2" 100, Server "Server3" 200]
+
+--number of servers ON (which we'll try to minimize)
+numberServersOn :: Map VMID SSID -> SInteger
+numberServersOn = count . elems . serverStates
+
+--A server is OFF iff no VM is allocated to it
+serverStates :: Map VMID SSID -> Map SID SBool
+serverStates sc = M.map (./= 0) (vmCounts sc)
+
+--compute the number of VMs on each servers
+vmCounts :: Map VMID SSID -> Map SID SInteger
+vmCounts vmls = M.mapWithKey count servers where
+   count sid _ = sum [ite (mysid .== literal sid) 1 0 | mysid <- elems vmls]
+
+--All the CPU constraints
+cpuConstraints :: Map VMID SSID -> SBool
+cpuConstraints vmls = bAnd $ elems $ M.mapWithKey criteria (serverCPUHeights vmls) where
+   criteria :: SID -> SInteger -> SBool
+   criteria sid height = (literal $ cpus $ fromJust $ M.lookup sid servers) .> height
+
+--gives the CPU consummed by VMs for each server
+serverCPUHeights :: Map VMID SSID -> Map SID SInteger 
+serverCPUHeights vmls = M.mapWithKey sumVMsHeights servers where
+   sumVMsHeights :: SID -> Server -> SInteger
+   sumVMsHeights sid _ = sum [ite (sid' .== literal sid) (literal $ cpuDemand $ fromJust $ M.lookup vmid vms) 0 | (vmid, sid') <- M.assocs vmls]
+
+--special version of minimize that works with our types
+myMinimize :: [VMID] -> (Map VMID SSID -> SInteger) -> (Map VMID SSID -> SBool) -> IO (Maybe (Map VMID SID))
+myMinimize vmids utility constraints = do
+   ms <- minimize' Quantified (utility . toVMLoc) (length vmids) (constraints . toVMLoc) 
+   return $ toVMLoc <$> ms where
+      toVMLoc :: [a] -> Map VMID a
+      toVMLoc = fromList . zip vmids
+
+minimize' = minimizeWith (defaultSMTCfg {verbose = True})
+
+--solves a VM placement problem
+vmPlacementProblem :: IO (Maybe (Map VMID SID))
+vmPlacementProblem = myMinimize (keys vms) numberServersOn cpuConstraints
+
+main = do
+   s <- vmPlacementProblem   
+   putStrLn $ show s
+
+
+-- count the true bits
+count :: [SBool] -> SInteger
+count []     = 0
+count (x:xs) = let c' = count xs in ite x (1+c') c'
+
